3300beafc1a584e1f2cae813be0837f0f5d60bc6
gamer-guangying <651104522@qq.com>
2025-05-30 15:03:38 +0800

    地图系统功能实现
    修改了MapPanel脚本，实现在不同场景打开对应的小地图，并绘制了地图背景板。

--- Assets/Scripts/MapSystem/MapPanel.cs
+++ Assets/Scripts/MapSystem/MapPanel.cs
@@ -9,15 +9,21 @@ public class MapPanel : abstractUIPanel
     [SerializeField] private Button returnButton;
     [SerializeField] private Transform mapButtonContainer;
     [SerializeField] private Button mapButtonPrefab;
+    [SerializeField] private Image miniMapImage; // 新增：小地图 Image 引用
+    [SerializeField] private Sprite myHomeMiniMap; // 新增：MyHome 小地图 Sprite
+    [SerializeField] private Sprite outdoorMiniMap; // 新增：Outdoor 小地图 Sprite
+
     private void Start()
     {
         returnButton.onClick.AddListener(OnReturnClicked);
     }
+
     public override void Open()
     {
         base.Open();
         Refresh();
     }
+
     override public void Refresh()
     {
         // 清空现有按钮
@@ -33,6 +39,9 @@ public class MapPanel : abstractUIPanel
         // 设置返回按钮状态
         returnButton.interactable = !string.IsNullOrEmpty(currentMap?.parentMap);
 
+        // 更新小地图显示
+        UpdateMiniMap(currentMap.mapName);
+
         // 创建可传送的地图按钮
         List<string> availableMaps = MapManager.Instance.GetAvailableMaps();
         foreach (string mapName in availableMaps)
@@ -45,11 +54,25 @@ public class MapPanel : abstractUIPanel
             mapButton.onClick.AddListener(() => OnMapSelected(mapName));
         }
     }
+
+    private void UpdateMiniMap(string mapName)
+    {
+        if (mapName == "MyHome")
+        {
+            miniMapImage.sprite = myHomeMiniMap;
+        }
+        else if (mapName == "Outdoor")
+        {
+            miniMapImage.sprite = outdoorMiniMap;
+        }
+    }
+
     private void OnReturnClicked()
     {
         MapManager.Instance.ReturnToParentMap();
         Close();
     }
+
     private void OnMapSelected(string mapName)
     {
         MapManager.Instance.SwitchToMap(mapName);
